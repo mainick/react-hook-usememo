7b767b4092461a70c376b05b3602a80cc91f7bbe
mainick <Ronnyx83>
2020-12-02 15:01:34 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useMemo } from "react";
+import { v4 as uuid } from "uuid";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [todos, setTodos] = useState([
+        { id: uuid(), title: "imparare React" },
+        { id: uuid(), title: "imparare gli state" },
+        { id: uuid(), title: "imparare i componenti funzionali" },
+    ]);
+    const [task, setTask] = useState("");
+    const [term, setTerm] = useState("");
+    const [search, setSearch] = useState("");
+
+    const onChangeTerm = (e) => {
+        setTerm(e.target.value);
+    };
+
+    const onChangeSeach = (e) => {
+        setSearch(term);
+    };
+
+    const onChangeTask = (e) => {
+        setTask(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (task.trim() === "") return;
+
+        setTodos([...todos, { title: task }]);
+        setTask("");
+    };
+
+    let filterTodos = useMemo(
+        () =>
+            todos.filter((todo) => {
+                console.log("esecuzione della funzione filtro");
+                return todo.title.toLowerCase().includes(search.toLowerCase());
+            }),
+        [search]
+    );
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    value={term}
+                    placeholder="cerca"
+                    onChange={onChangeTerm}
+                />
+                <button type="button" onClick={onChangeSeach}>
+                    cerca
+                </button>
+            </form>
+            <ListTodos todos={filterTodos} />
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    value={task}
+                    placeholder="aggiungi todo"
+                    onChange={onChangeTask}
+                />
+            </form>
+        </div>
+    );
+};
+
+const ListTodos = ({ todos }) => {
+    return (
+        <ul>
+            {todos.map((item, i) => (
+                <Todo key={i} todo={item} />
+            ))}
+        </ul>
+    );
+};
+
+const Todo = ({ todo }) => {
+    return <li key={todo.id}>{todo.title}</li>;
+};
 
 export default App;
